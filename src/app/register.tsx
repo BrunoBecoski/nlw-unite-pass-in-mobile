409cb0630dff2cf8b404f396f94d5c01a8d28365
Bruno Becoski <brunobecoski@gmail.com>
2024-04-06 16:24:17 -0300

    Cria a funcionalidade de inscrever o usuário

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,22 +1,55 @@
 import { useState } from 'react'
 import { Alert, Image, StatusBar, View } from 'react-native'
+import axios from 'axios'
 import { Link, router } from 'expo-router'
 import { FontAwesome6, MaterialIcons } from '@expo/vector-icons'
 
+import { api } from '@/server/api'
 import { Input } from '@/components/input'
 import { Button } from '@/components/button'
 import { colors } from '@/styles/colors'
 
+const EVENT_ID = 'cb9108f2-8d99-4d30-bfa1-bb6e3bb41da0'
+
 export default function Register() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
-  function handleRegister() {
-    if (!name.trim() || !email.trim()) {
-      return Alert.alert('Inscrição', 'Preencha todos os campos!')
+  async function handleRegister() {
+    try {
+      if (!name.trim() || !email.trim()) {
+        return Alert.alert('Inscrição', 'Preencha todos os campos!')
+      }
+      
+      setIsLoading(true)
+  
+      const registerResponse = await api.post(`/events/${EVENT_ID}/attendees`, {
+        name,
+        email,
+      })
+
+      if (registerResponse.data.attendeeId) {
+        Alert.alert('Inscrição', 'Inscrição realizado com sucesso!',[
+          {
+            text: 'OK',
+            onPress: () => router.push('/ticket')
+          }
+        ])
+      }
+    } catch (error) {
+      console.log(error)
+
+      if (axios.isAxiosError(error)) {
+        if (String(error.response?.data.message).includes('already registered')) {
+          return Alert.alert('Inscrição', 'Este-email já está cadastrado!')
+        }
+      }
+
+      Alert.alert('Inscrição', 'Não foi possível fazer a inscrição!')
+    } finally {
+      setIsLoading(false)
     }
-
-    router.push('/ticket')
   }
 
   return (
@@ -38,7 +71,7 @@ export default function Register() {
           />
           <Input.Field onChangeText={setName} placeholder="Nome completo" />
         </Input>
-
+ 
         <Input>
           <MaterialIcons
             name="alternate-email"
@@ -52,7 +85,11 @@ export default function Register() {
           />
         </Input>
 
-        <Button onPress={handleRegister} title="Realizar inscrição" />
+        <Button 
+          onPress={handleRegister}
+          title="Realizar inscrição"
+          isLoading={isLoading}
+        />
 
         <Link href="/" className="text-gray-100 text-base font-bold text-center mt-8">
           Já possui ingresso?
